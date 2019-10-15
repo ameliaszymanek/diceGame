d23d68c176ecde4fa47f1b6ad57bfd8f1953b404
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-10-15 16:01:56 -0500

    Added while loop that allows player 1 to have a turn then player 2, until one has score of 100. Game works and is able to run completely.

--- eelsEscalatorElevator.js
+++ eelsEscalatorElevator.js
@@ -1,129 +1,139 @@
 
 
-//*set up*
-
-function runGame(){
-//set up  -ONCE
-    let player1Score = 0;
-    let player2Score = 0;
-
-//gameplay loop- with while loop -OVER, etc.
-    let continueGame = true
-    
-    while(continueGame === true){
-        if (player1Score > 100 && player2Score > 100){
-            rollDie(n)++;
-    }
-    else()
-        
-    
-
-    }
-//cleanup/ end game -ONCE
-
-
-
-}
-
-
-
 //roll dice function
 function rollDie(n){
     return (Math.floor(Math.random() * n) + 1);
 }
 
-function rollDice(){
-
+function runGame(){
+    let player1Score = 0;
+    let player2Score = 0;
     let p1Turn = true;
+
 // player 1 turn
-    if(p1Turn === true){
+    while(player1Score < 100 && player2Score < 100){
 
-        let i = player1Score
-        let playerMove = rollDie();
-        
-        if(i <= 19){
-            playerMove = rollDie(6);
-            player1Score += playerMove;
-            console.log("Player 1, you rolled a " + playerMove + " Your current spot is " + player1Score);
+
+        if(p1Turn === true){
+
+            let i = player1Score
+            let playerMove = rollDie();
+            
+            if(i <= 19){
+                playerMove = rollDie(6);
+                player1Score += playerMove;
+                console.log("Player 1, you rolled a " + playerMove + " Your current spot is " + player1Score);
+                p1Turn = false;
+            }
+
+            else if(i >= 20 && i<= 39){
+                playerMove = rollDie(8);
+                player1Score += playerMove;
+                console.log("Player 1, you rolled a " + playerMove + " Your current spot is " + player1Score);
+                p1Turn = false;
+            }  
+
+            else if(i >= 40 && i <= 59){
+                playerMove = rollDie(10);
+                player1Score += playerMove;
+                console.log("Player 1, you rolled a " + playerMove + " Your current spot is " + player1Score);
+                p1Turn = false;
+            }        
+
+            else if(i >= 60 && i <= 79){
+                playerMove = rollDie(12);
+                player1Score += playerMove;
+                console.log("Player 1, you rolled a " + playerMove + " Your current spot is " + player1Score);
+                p1Turn = false;
+            }    
+
+            else if(i >= 80 && i <= 99){
+                playerMove = rollDie(14);
+                if(player1Score + playerMove > 100){
+                    player1Score = 100 - (player1Score + playerMove - 100)   
+                }
+                else{
+                    player1Score += playerMove;
+                }
+             
+                console.log("Player 1, you rolled a " + playerMove + " Your current spot is " + player1Score);
+                
+                p1Turn = false;
+            }
+
+            // // not sure if this is written out correctly
+            // else if(i == 9 || i == 34 || i == 41 || i == 67 || i == 75|| i == 82 || i == 97){
+            //     playerMove = rollDie(20);
+            //     player1Score += playerMove;
+            //     console.log("Player 1, you rolled a " + playerMove + " Your current spot is " + player1Score);
+            //     p1Turn = false;
+            // }
         }
 
-        else if(i >= 20 && i<= 39){
-            playerMove = rollDie(8);
-            player1Score += playerMove;
-            console.log("Player 1, you rolled a " + playerMove + " Your current spot is " + player1Score);
-        }  
+    // player 2 turn
+        else{
 
-        else if(i >= 40 && i <= 59){
-            playerMove = rollDie(10);
-            player1Score += playerMove;
-            console.log("Player 1, you rolled a " + playerMove + " Your current spot is " + player1Score);
-        }        
+            let i = player2Score;
+            let playerMove = rollDie();
 
-        else if(i >= 60 && i <= 79){
-            playerMove = rollDie(12);
-            player1Score += playerMove;
-            console.log("Player 1, you rolled a " + playerMove + " Your current spot is " + player1Score);
-        }    
+            if(i <= 19){
+                playerMove = rollDie(6);
+                player2Score += playerMove;
+                console.log("Player 2, you rolled a " + playerMove+ " Your current spot is " + player2Score);
+                p1Turn = true;
+            }   
+            
+            else if(i >=20 && i<= 39){
+                playerMove = rollDie(8);
+                player2Score += playerMove;
+                console.log("Player 2, you rolled a " + playerMove+ " Your current spot is " + player2Score);
+                p1Turn = true;
+            }  
 
-        else if(i >= 80 && i <= 99){
-            playerMove = rollDie(14);
-            player1Score += playerMove;
-            console.log("Player 1, you rolled a " + playerMove + " Your current spot is " + player1Score);
+            else if(i >= 40 && i <= 59){
+                playerMove = rollDie(10);
+                player2Score += playerMove;
+                console.log("Player 2, you rolled a " + playerMove+ " Your current spot is " + player2Score);
+                p1Turn = true;
+            }   
+
+            else if(i >= 60 && i <= 79){
+                playerMove = rollDie(12);
+                player2Score += playerMove;
+                console.log("Player 2, you rolled a " + playerMove+ " Your current spot is " + player2Score);
+                p1Turn = true;
+            }   
+
+            else if(i >= 80 && i <= 99){
+                playerMove = rollDie(14);
+                if(player2Score + playerMove > 100){
+                    player2Score = 100 - (player2Score + playerMove - 100)   
+                }
+                else{
+                    player2Score += playerMove;
+                }
+                console.log("Player 2, you rolled a " + playerMove+ " Your current spot is " + player2Score);
+                p1Turn = true;
+            }
+
+            // //not sure if this is written out correctly 
+            // else if(i == 9 || i == 34 || i == 41 || i == 67 || i == 75|| i == 82 || i == 97){
+            //     playerMove = rollDie(20);
+            //     player1Score += playerMove;
+            //     console.log("Player 2, you rolled a " + playerMove+ " Your current spot is " + player2Score);
+            //     p1Turn = true;
+            // }
         }
-
-        // not sure if this is written out correctly
-        else if(i = 9, 34, 41, 67, 75, 82, 97){
-            playerMove = rollDie(20);
-            player1Score += playerMove;
-            console.log("Player 1, you rolled a " + playerMove + " Your current spot is " + player1Score);
-        }
-        // have to write something if i = 100 saying they won
     }
-
-// player 2 turn
-    else{
-
-        let i = player2Score;
-        let playerMove = rollDie();
-
-        if(i <= 19){
-            playerMove = rollDie(6);
-            player2Score += playerMove;
-            console.log("Player 2, you rolled a " + playerMove+ " Your current spot is " + player2Score);
-        }   
-        
-        else if(i >=20 && i<= 39){
-            playerMove = rollDie(8);
-            player2Score += playerMove;
-            console.log("Player 2, you rolled a " + playerMove+ " Your current spot is " + player2Score);
-        }  
-
-        else if(i >= 40 && i <= 59){
-            playerMove = rollDie(10);
-            player2Score += playerMove;
-            console.log("Player 2, you rolled a " + playerMove+ " Your current spot is " + player2Score)
-        }   
-
-        else if(i >= 60 && i <= 79){
-            playerMove = rollDie(12);
-            player2Score += playerMove;
-            console.log("Player 2, you rolled a " + playerMove+ " Your current spot is " + player2Score);
-        }   
-
-        else if(i >= 80 && i <= 99){
-            playerMove = rollDie(14);
-            player2Score += playerMove;
-            console.log("Player 1, you rolled a " + playerMove+ " Your current spot is " + player2Score);
-        }
-
-        //not sure if this is written out correctly 
-        else if(i = 9, 34, 41, 67, 75, 82, 97){
-            playerMove = rollDie(20);
-            player1Score += playerMove;
-            console.log("Player 2, you rolled a " + playerMove+ " Your current spot is " + player2Score);
-        }
-         // have to write something if i = 100 saying they won
+    //cleanup/ end game -ONCE
+    if(player1Score == 100){
+        console.log("You did it, you really won, Player 1!");
     }
+    if(player2Score == 100){
+        console.log("You did it, you really won, Player 2!");
+    }
+    
+
 }
 
 //*clean up/ end game*
